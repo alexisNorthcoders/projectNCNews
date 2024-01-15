6aa33157dcb07ab80febd81eaf468f5fd99f1f4a
alexisNorthCoders <alexiscreoulo@hotmail.com>
2024-01-15 14:19:22 +0000

    add GET /api

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const endpoints = require("./endpoints.json")
 
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
 
 app.get("/api", (req,res,next) => {
-    res.status(200).send(endpoints)
+    res.status(200).send({endpoints})
     next()
    
 })
 
 app.all("/*", (req, res, next) => {
     res.status(404).send({ msg: "Path not found" });
-    next()
+  
 })
 
-app.use((err, req, res,next) => {
 
-});
 
 module.exports = app;
