1533738fdd94e08ae2abbbd3431458745cc4ae4a
alexisNorthCoders <alexiscreoulo@hotmail.com>
2024-02-10 13:39:46 +0000

    change connection to use elfhosted server

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'production'; // change this to development to use local database
+const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
